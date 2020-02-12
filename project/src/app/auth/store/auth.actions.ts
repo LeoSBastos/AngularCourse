1c94d242d01ecd6e2e640ab27f501c3c90fb45ec
Leo Bastos <leonardo.bastos@senado.leg.br>
2020-02-12 14:02:02 -0300

    NgRx

--- project/src/app/auth/store/auth.actions.ts
+++ project/src/app/auth/store/auth.actions.ts
@@ -3,6 +3,8 @@ import { Action } from '@ngrx/store';
 export const LOGIN_START = '[Auth] Login Start';
 export const LOGIN = '[Auth] Login';
 export const LOGIN_FAIL = '[Auth] Login Fail';
+export const SIGNUP_START = '[Auth] Signup Start';
+export const SIGNUP_FAIL = '[Auth] Signup Fail';
 export const LOGOUT = '[Auth] Logout';
 
 export class Login implements Action {
@@ -15,23 +17,40 @@ export class Login implements Action {
       token: string;
       expirationDate: Date;
     }
-  ) { }
+  ) {}
+}
+
+export class LoginStart implements Action {
+  readonly type = LOGIN_START;
+
+  constructor(public payload: { email: string; password: string }) {}
+}
+
+export class LoginFail implements Action {
+  readonly type = LOGIN_FAIL;
+
+  constructor(public payload: string) {}
+}
+
+export class SignupStart implements Action {
+  readonly type = SIGNUP_START;
+
+  constructor(public payload: { email: string; password: string }) {}
+}
+
+export class SignupFail implements Action {
+  readonly type = SIGNUP_FAIL;
+  constructor(public payload: string) {}
 }
 
 export class Logout implements Action {
   readonly type = LOGOUT;
 }
 
-export class LoginStart implements Action {
-  readonly type = LOGIN_START;
-
-  constructor(public payload: { email: string; password: string }) { }
-}
-
-export class LoginFail implements Action {
-  readonly type = LOGIN_FAIL;
-
-  constructor(public payload: string) { }
-}
-
-export type AuthActions = Login | Logout | LoginStart | LoginFail;
+export type AuthActions =
+  | Login
+  | Logout
+  | LoginStart
+  | LoginFail
+  | SignupStart
+  | SignupFail;
